ed76403e4c946678bec73a49536c69cd39810af2
Giancarlo <gfabrizi@gmail.com>
2016-08-09 17:13:23 +0200

    feature test: create a space

--- spec/features/create_space_spec.rb
+++ spec/features/create_space_spec.rb
@@ -1,11 +1,11 @@
 feature "Creation of spaces" do
   scenario "creates a space" do
     visit "/spaces"
-    click_link "create a space" 
+    click_link "create a space"
     fill_in :name, with: "Nice Place"
     fill_in :description, with: "Very nice place"
     fill_in :price, with: "136.50"
-    click_button :submit
+    click_button "Create"
     expect(page).to have_content("Nice Place")
   end
 end
